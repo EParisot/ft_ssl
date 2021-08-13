c7d0a21371bcc76679b11db37666b124a8cb239b
eparisot <eparisot@student.42.fr>
2021-08-13 20:20:25 +0200

    tests ok

--- tests/ssh_tests.sh
+++ tests/ssh_tests.sh
@@ -1,13 +1,51 @@
 #! /bin/bash
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}FT_SSL Tests:${NC}"; echo
+echo -e "${YELLOW}Compiling...${NC}"
 make
+echo
+
+echo -e "${YELLOW}Simple Tests${NC}:"; echo
 
 HASHES=(md5 sha224 sha256 base64)
-TEST_STRINGS=("" "abc" "bonjour42" "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-echo "Simple Tests :"
-for H in ${HASHES[@]}; do for S in ${TEST_STRINGS[@]}; do echo -n "$S" | ./ft_ssl $H; echo -n "$S" | openssl $H; done; done
+TEST_STRINGS=("" \
+			"abc" \
+			"bonjour42" \
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+)
+
+for H in ${HASHES[@]} 
+	do for S in ${TEST_STRINGS[@]}
+		do echo -e "${BLUE}./ft_ssl ${H} ${S}${NC}:"
+		echo -n "$S" | ./ft_ssl $H; echo -e "${BLUE}openssl ${H} ${S}${NC}:"
+		echo -n "$S" | openssl $H | sed 's/(stdin)= //'
+		echo
+	done
+done
+
+echo -e "${YELLOW}Decode Tests${NC}:"; echo
 
 HASHES=(base64)
-base64_STRINGS=("" "YWJj" "Ym9uam91cjQy" "YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYQ==")
-echo "Decrypt Tests :"
-for H in ${HASHES[@]}; do var=${H}_STRINGS[@]; for S in ${!var}; do echo "$S" | ./ft_ssl $H -d; echo; echo "$S" | openssl $H -d ; echo; done; done
+base64_STRINGS=("" \
+			"YWJj" \
+			"Ym9uam91cjQy" \
+			"YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYQ==" \
+)
+
+for H in ${HASHES[@]}
+	do var=${H}_STRINGS[@]
+	for S in ${!var}
+		do echo -e "${BLUE}./ft_ssl ${H} -d ${S}${NC}:"
+		echo "$S" | ./ft_ssl $H -d; echo
+		echo -e "${BLUE}openssl ${H} -d ${S}${NC}:"
+		echo "$S" | openssl $H -d ; echo
+		echo
+	done
+done
